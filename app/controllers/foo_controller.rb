64b0033b08810664c8080546407a40f52a3718ee
anujmshrestha <anujshrestha15@gmail.com>
2017-03-30 14:41:27 +0545

    action added in controller which converts into json,xml and html (basics)

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -1,4 +1,11 @@
 class FooController < ApplicationController
-  def film
+def film
+ 		respond_to do |format|
+		msg={:status => "Title", :message =>"2016/12/11"}
+  			format.json{render :json => msg}
+  			format.xml {render :xml =>msg }
+  			format.html{render :html =>msg }
+		end
   end
+
 end
