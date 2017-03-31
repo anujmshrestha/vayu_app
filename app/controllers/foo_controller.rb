97428a7bf7fcdd3c713372ac1f1b0cb66fb978e7
anujmshrestha <anujshrestha15@gmail.com>
2017-03-31 09:49:25 +0545

    csv filepath added

--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -1,11 +1,32 @@
 class FooController < ApplicationController
 def film
- 		respond_to do |format|
-		msg={:status => "Title", :message =>"2016/12/11"}
-  			format.json{render :json => msg}
-  			format.xml {render :xml =>msg }
-  			format.html{render :html =>msg }
+ 		require 'csv' 
+    filename="public/Film.csv"
+    #CSV.foreach(filename) do |row|
+     # puts row[2]
+    #end
+    random_line = nil
+    File.open("public/Film.csv") do |file|
+    file_lines = file.readlines()
+    random_line = file_lines[Random.rand(0...file_lines.size())]
+    end
+    x=random_line.split(',') 
+   # puts random_line
+    respond_to do |format|
+		    @msg={:Title => x[1], :Year=>x[2]}
+  			 format.json{render :json => @msg}
+  			 format.xml {render :xml => @msg }
+  			 format.html{render :html => @msg }
 		end
+
   end
+ 
+  def home
+  	@msg= 'Hello world'
+
+  end
+
+  	
+  
 
 end
